16f44c64752613301ea837408ae3105219524f40
lis5662 <lis5662@gmail.com>
2015-11-02 11:43:53 +0200

    Registration page HTML/CSS

---
+++ Index.php
@@ -0,0 +1,24 @@
+<!Doctype html>
+<html>
+    <head>
+       <meta charset="utf-8">
+        <title>Registretion form</title>
+        <link rel="stylesheet" href="style.css">
+       
+    </head>
+    <body>
+     <h1>Create your account on "Chicago Winter"</h1>
+     
+     <form action=" https://www.google.com" method="post">
+         <label>Your login (email)</label>:<br>
+         <input type="email" name="email"><br>
+         <label>Your password</label>:<br>
+         <input type="password" name="password"><br>
+         <label>Information about you</label>:<br>
+         <textarea rows="3" cols="20"></textarea><br>
+         <input type="checkbox" checked>
+         <label>Sign up for news</label><br>
+         <input type="submit"class="button" value="Create" <button style="width:50x;height:55px" title="Registration" font-size="15px"></button>
+    </form>
+    </body>
+</html>
